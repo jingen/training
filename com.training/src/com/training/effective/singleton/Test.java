4d8a5809bf8fac8d3ee40bd6d5e896f75220688b
Jingen Lin <jingen.lin.jl@gmail.com>
2014-09-12 17:16:07 -0400

    prefer primitives to boxed primitives, and watch out for unintentional autoboxing

--- com.training/src/com/training/effective/singleton/Test.java
+++ com.training/src/com/training/effective/singleton/Test.java
@@ -6,11 +6,21 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s = "stringette";
-		System.out.println(s);
-		Case c = new Case();
-		System.out.println(c.getName());
-		System.out.println(c.getAge());
+//		String s = "stringette";
+//		System.out.println(s);
+//		Case c = new Case();
+//		System.out.println(c.getName());
+//		System.out.println(c.getAge());
+		long start = System.currentTimeMillis();
+		Long sum = 0L;
+//      1416(long) : 7960(Long)
+//		prefer primitives to boxed primitives, and watch out for unintentional autoboxing
+		for(long i=0; i < Integer.MAX_VALUE; i++){
+			sum += i;
+		}
+		System.out.println(sum);
+		long end= System.currentTimeMillis();
+		System.out.println(end - start);
 	}
 
 }
