b2be6a0787825198f45599f36e468b9961b84203
Jingen Lin <jingen.lin.jl@gmail.com>
2014-11-24 23:06:47 -0500

    two sum 2

--- com.training/src/com/training/first/TwoSum2.java
+++ com.training/src/com/training/first/TwoSum2.java
@@ -1,9 +1,9 @@
 package com.training.first;
 
 public class TwoSum2 {
-	private static final int NO_FOUND = -1;
+	public static final int NO_FOUND = -1;
 	
-	private static int binarySearch(int[] numbers, int key, int start, int end){
+	public static int binarySearch(int[] numbers, int key, int start, int end){
 		while(start<=end){
 			int middle = start + (end-start)/2;
 			if(numbers[middle] == key){
@@ -17,9 +17,9 @@ public class TwoSum2 {
 		return NO_FOUND;
 	}
 	
-	private static int[] findSum(int[] numbers, int target){
+	public static int[] findSum(int[] numbers, int target){
 		for(int i=0; i<numbers.length; i++){
-			int result = binarySearch(numbers, target - numbers[i], 0, numbers.length -1);
+			int result = binarySearch(numbers, target - numbers[i], i+1, numbers.length -1);
 			if(result != NO_FOUND){
 				return new int[] { i+1, result + 1};
 			}
@@ -27,14 +27,26 @@ public class TwoSum2 {
 		throw new IllegalArgumentException("No two sum solution.");
 	}
 	
+	public static int[] findSum2(int[] numbers, int target){
+		int i=0, j=numbers.length-1; 
+		while(i<j){
+			if(numbers[i] + numbers[j] == target){
+				return new int[] { i+1, j+1 };
+			}else if(numbers[i] + numbers[j] > target){
+				j -= 1;
+			}else{
+				i += 1;
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution.");
+	}
+	
 	public static void main(String[] args){
 		int[] testArray = {1, 3, 7, 8, 9, 12, 13, 17};
-		int[] result = findSum(testArray, 17);
+//		int[] result = findSum(testArray, 17);
+		int[] result = findSum2(testArray, 17);
 		System.out.println(result[0]);
 		System.out.println(result[1]);
-		System.out.println("End");
-//		int result = binarySearch(testArray, 12, 0, testArray.length-1);
-//		System.out.println(result);
 		
 	}
 }
