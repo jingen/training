d89eaaccbdfe5e039694533cec397a25e9bfc451
Jingen Lin <jingen.lin.jl@gmail.com>
2014-12-06 20:03:13 -0500

    Read N Character Given Read 4 2 -- finished

--- com.training/src/com/training/first/ReadNGivenRead4.java
+++ com.training/src/com/training/first/ReadNGivenRead4.java
@@ -28,8 +28,27 @@ public class ReadNGivenRead4 {
 		while(!eof && readBytes<n){
 			int sz = read4(buffer);
 			if(sz<4) eof=true;
-			int bytes = Math.max(sz, n-readBytes);
+			int bytes = Math.min(sz, n-readBytes);
 			System.arraycopy(buffer, 0, buf, readBytes, bytes);
+			readBytes += bytes;
+		}
+		return readBytes;
+	}
+	
+	private char[] buffer = new char[4];
+	int offset=0, bufsize=0;
+	public int read3(char[] buf, int n){
+		int readBytes = 0;
+		boolean eof = false;
+		while(!eof && readBytes<n){
+			if(bufsize==0){
+				bufsize = read4(buffer);
+				eof = bufsize<4;
+			}
+			int bytes = Math.min(n-readBytes, bufsize);
+			System.arraycopy(buffer, offset, buf, readBytes, bytes);;
+			offset=(offset + bytes)%4;
+			bufsize -= bytes;
 			readBytes += bytes;
 		}
 		return readBytes;
--- com.training/src/com/training/first/ReadNGivenRead42.java
+++ com.training/src/com/training/first/ReadNGivenRead42.java
@@ -2,4 +2,35 @@ package com.training.first;
 
 public class ReadNGivenRead42 {
 
+	private char[] buffer = new char[4];
+	int offset = 0, bufsize = 0;
+
+	/**
+	 * @param buf
+	 *            Destination buffer
+	 * @param n
+	 *            Maximum number of characters to read
+	 * @return The number of characters read
+	 */
+	private int read4(char[] buf) {
+		return 4;
+	}
+
+	public int read(char[] buf, int n) {
+		int readBytes = 0;
+		boolean eof = false;
+		while (!eof && readBytes < n) {
+			if (bufsize == 0) {
+				bufsize = read4(buffer);
+				eof = bufsize < 4;
+			}
+			int bytes = Math.min(n - readBytes, bufsize);
+			System.arraycopy(buffer /* src */, offset /* srcPos */, buf /* dest */,
+					readBytes /* destPos */, bytes /* length */);
+			offset = (offset + bytes) % 4;
+			bufsize -= bytes;
+			readBytes += bytes;
+		}
+		return readBytes;
+	}
 }
