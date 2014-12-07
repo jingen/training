aae731d1e109e38dcdaae7e62f3ca385116f5150
Jingen Lin <jingen.lin.jl@gmail.com>
2014-12-07 12:12:06 -0500

    reverse integer

--- com.training/src/com/training/first/ReverseInteger.java
+++ com.training/src/com/training/first/ReverseInteger.java
@@ -2,5 +2,23 @@ package com.training.first;
 
 public class ReverseInteger {
 
+	public static final int BOUNDARY = Integer.MAX_VALUE/10;
+	public static int reverse(int aInt){
+		int result=0;
+//		while(aInt>0){ -- have to consider negative numbers
+		while(aInt!=0){
+			if(Math.abs(result) > BOUNDARY) return 0;
+			result = result*10 + aInt%10;
+			aInt = aInt/10;
+		}
+		return result;
+	}
 	
+	public static void main(String[] args){
+		int x = 1230;
+		System.out.println(reverse(x));
+		x = -1230;
+		System.out.println(reverse(x));
+		System.out.println(reverse(Integer.MAX_VALUE));
+	}
 }
