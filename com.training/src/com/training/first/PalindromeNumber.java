fc021f0a49a029b6ab5db9482baced24303dee44
Jingen Lin <jingen.lin.jl@gmail.com>
2014-12-08 16:42:10 -0500

    Palindrome number

--- com.training/src/com/training/first/PalindromeNumber.java
+++ com.training/src/com/training/first/PalindromeNumber.java
@@ -16,10 +16,29 @@ public class PalindromeNumber {
 		int reversedNumber = reverse(number);
 		return reversedNumber == number;
 	}
+	
+	public static boolean isPanlindrome2(int number){
+		int div = 1;
+		while(number/div>=10){
+			div *= 10;
+		}
+		while(number>=10){
+			int head = number/div;
+			int tail = number%10;
+			if(head!=tail) return false;
+			number = number%div/10;
+			div /= 100;
+		}
+		return true;
+	}
 	public static void main(String[] args){
-//		System.out.println(isPanlindrome(9361));
-//		System.out.println(isPanlindrome(1639361));
+		System.out.println(isPanlindrome(9361));
+		System.out.println(isPanlindrome2(9361));
+		System.out.println(isPanlindrome(1639361));
+		System.out.println(isPanlindrome2(1639361));
 //		reverse(-33);
 		System.out.println(isPanlindrome(Integer.MAX_VALUE));
+		System.out.println(isPanlindrome2(Integer.MAX_VALUE));
+		System.out.println(isPanlindrome2(89733798));
 	}
 }
