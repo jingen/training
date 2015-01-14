c479099a295b2f60d9fd9faf6b36a304269f2764
Jingen Lin <jingen.lin.jl@gmail.com>
2015-01-14 17:03:13 -0500

    Valid parentheses -- done

--- com.training/src/com/training/first/ValidParentheses.java
+++ com.training/src/com/training/first/ValidParentheses.java
@@ -1,6 +1,31 @@
 package com.training.first;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class ValidParentheses {
 
-	
+	private static final Map<Character, Character> parentheses = new HashMap<Character, Character>() {
+
+		private static final long serialVersionUID = 1L;
+
+		{
+			put('(', ')');
+			put('[', ']');
+			put('{', '}');
+		}
+	};
+
+	public boolean isValid(String s){
+		Stack<Character> stack = new Stack<>();
+		for(char c: s.toCharArray()){
+			if(parentheses.containsKey(c)){
+				stack.push(c);
+			}else{
+				if(stack.isEmpty() || parentheses.get(stack.pop()) != c) return false;
+			}
+		}
+		return stack.isEmpty();
+	}
 }
