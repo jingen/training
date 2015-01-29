29f36ce91dcc02c5ba222154b45c2087957f4523
Jingen Lin <jingen.lin.jl@gmail.com>
2015-01-29 17:19:39 -0500

    Find Minimum Element in a Sorted Rotated Array. -- done

---
+++ com.training/src/com/training/first/FindMinInSortedRotatedArray.java
@@ -0,0 +1,25 @@
+package com.training.first;
+
+public class FindMinInSortedRotatedArray {
+
+	public int find(int[] A){
+		int L=0, R=A.length-1;
+		while(L<R && A[L]>A[R]){
+			int M = (L+R)/2;
+			if(A[M] > A[R]){
+				L = M + 1;
+			}else{
+				R = M;
+			}
+		}
+		return A[L];
+	}
+
+	public static void main(String[] args){
+		int[] A = {5, 6, 7, 0, 1, 2, 3, 4};
+		int[] B = {4, 5, 6, 7, 0, 1, 2};
+		FindMinInSortedRotatedArray f = new FindMinInSortedRotatedArray();
+		System.out.println(f.find(A));
+		System.out.println(f.find(B));
+	}
+}
