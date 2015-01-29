723127e1ddd0023b859dd8e0c861c85ed9fba88d
Jingen Lin <jingen.lin.jl@gmail.com>
2015-01-29 17:34:14 -0500

    find min in sorted rotated array 2 -- done

--- com.training/src/com/training/first/FindMinInSortedRotatedArray.java
+++ com.training/src/com/training/first/FindMinInSortedRotatedArray.java
@@ -14,6 +14,21 @@ public class FindMinInSortedRotatedArray {
 		}
 		return A[L];
 	}
+	
+	public int find2(int[] A){
+		int L=0, R=A.length-1;
+		while(L<R && A[L]>=A[R]){
+			int M = (L+R)/2;
+			if(A[M]>A[R]){
+				L = M+1;
+			}else if(A[M]<A[R]){
+				R = M;
+			}else{
+				L = L+1;
+			}
+		}
+		return A[L];
+	}
 
 	public static void main(String[] args){
 		int[] A = {5, 6, 7, 0, 1, 2, 3, 4};
